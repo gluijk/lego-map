c5ee1938f9f4496ee2ffa39b7f8c5765051d0be2
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2024-01-23 02:58:13 +0100

    Add files via upload

--- legomap.R
+++ legomap.R
@@ -38,33 +38,115 @@ brick[1, 45]=LIGHT
 brick[46, 2]=DARK
 
 
+name=c('retrato', 'beso', 'guadarrama', 'girasoles', 'europe', 'world')
+K=c(8, 4, 7, 8, 6, 10)  # k-means clusters
+
+name=c('africa')
+K=c(10)  # k-means clusters
+
 LEGOSIZE=60  # output vertical size (number of LEGO bricks)
-for (name in c('retrato', 'beso', 'guadarrama', 'girasoles')) {
-    print(name)
-    img=readPNG(paste0(name, ".png"))
+
+# Pipeline: img (input) -> imglite (downsized) -> imgclust (clustered)
+for (n in 1:length(name)) {
+    print(paste0("Resizing, clustering (k=", K[n], ") and LEGOing '",
+                 name[n], "'..."))
+    
+    ################################
+    # 1. DOWNSIZE IMAGE
+    
+    img=readPNG(paste0(name[n], ".png"))
     DIMY=nrow(img)
     DIMX=ncol(img)
     
     imglite=arrayresample(img, round(LEGOSIZE*DIMX/DIMY), LEGOSIZE)
-    writePNG(imglite, paste0(name, "lite.png"))
+    writePNG(imglite, paste0(name[n], "_lite.png"))
+
     
-    DIMY=nrow(imglite)
+    ################################
+    # 2. K-MEANS CLUSTERING
+    
+    # MEDIAN AVERAGING
     DIMX=ncol(imglite)
-    imgout=array(0, c(DIMY*BRICKSIZE, DIMX*BRICKSIZE, 3))
+    DIMY=nrow(imglite)
+
+    R=imglite[,,1]
+    G=imglite[,,2]
+    B=imglite[,,3]
+    dim(R)=length(R)
+    dim(G)=length(G)
+    dim(B)=length(B)
     
-    imglite[imglite>0.95]=0.95  # clip highlights to prevent whitening
-    imglite[imglite<0.05]=0.05  # clip shadows to prevent blackening
+    M=array(0, c(length(R), 3))  # Matriz RGB
+    colnames(M)=c("R","G","B")
+    M[,1]=R
+    M[,2]=G
+    M[,3]=B
+
+    # K-MEANS CLUSTERING
+    NCOLOURS=K[n]  # k clusters
+    set.seed(0)  # reproducible clustering
+    kmeansfit=kmeans(subset(M, select=c("R","G","B")), centers=NCOLOURS,
+                     nstart=1000, iter.max=500)  # high nstart can prevent from
+    clustering=kmeansfit$cluster           # missing the tiniest clusters
+    centers=kmeansfit$centers
+    
+    # Clustering histogram
+    png(paste0(name[n],"_histogram.png"), width=512, height=400)
+    breaks=seq(0, NCOLOURS, length.out=NCOLOURS+1)
+    colores=c()
+    for (h in 1:NCOLOURS) {
+        colores=c(colores, rgb(centers[h,1], centers[h,2], centers[h,3]))
+    }
+    hist(clustering, breaks=breaks, col=colores, # lty="blank",
+         main=paste0("'",name[n],"' cluster histogram (k=", K[n], ")"), axes=FALSE)
+    axis(1, at=breaks, labels=TRUE)
+    dev.off()
+    
+    # Clustered image
+    imgcentersR=array(0, DIMY*DIMX)
+    imgcentersG=imgcentersR
+    imgcentersB=imgcentersR
+    for (i in 1:NCOLOURS) {
+        indices=which(clustering==i)
+        imgcentersR[indices]=centers[i,1]
+        imgcentersG[indices]=centers[i,2]
+        imgcentersB[indices]=centers[i,3]
+    }
+    dim(imgcentersR)=c(DIMY,DIMX)
+    dim(imgcentersG)=c(DIMY,DIMX)
+    dim(imgcentersB)=c(DIMY,DIMX)
+    
+    imgclust=array(0, c(DIMY,DIMX,3))
+    imgclust[,,1]=imgcentersR
+    imgclust[,,2]=imgcentersG
+    imgclust[,,3]=imgcentersB
+    # writeTIFF(imgclust, paste0(name[n],"_clustered.tif"),
+    #           bits.per.sample=16, compression="LZW")
+    writePNG(imgclust, paste0(name[n],"_lite_clustered.png"))
+
+    
+    ################################
+    # 3. BUILD FINAL IMAGE WITH LEGO BRICKS
+
+    DIMY=nrow(imgclust)
+    DIMX=ncol(imgclust)
+    imgout=array(0, c(DIMY*BRICKSIZE, DIMX*BRICKSIZE, 3))
+
+    imgclust[imgclust>0.95]=0.95  # clip highlights to prevent whitening
+    imgclust[imgclust<0.05]=0.05  # clip shadows to prevent blackening
     for (i in 1:DIMY) {
         for (j in 1:DIMX) {
             for (chan in 1:3) {
                 # This gamma turns brick median (~0.5) into the pixel colour
-                gamma=1/(log(imglite[i, j, chan])/log(MIDGRAY))
+                gamma=1/(log(imgclust[i, j, chan])/log(MIDGRAY))
                 # Vectorized colouring for single brick RGB channel
                 imgout[((i-1)*BRICKSIZE+1):(i*BRICKSIZE),
                        ((j-1)*BRICKSIZE+1):(j*BRICKSIZE), chan]=brick^(1/gamma)
             }
         }
     }
-    # writeTIFF(imgout, paste0(name, "_lego.tif"), bits.per.sample=16, compression="LZW")
-    writePNG(imgout, paste0(name, "_lego.png"))
+    # writeTIFF(imgout, paste0(name[n], "_lego.tif"), bits.per.sample=16, compression="LZW")
+    writePNG(imgout, paste0(name[n], "_lego_degraded.png"))
 }
+
+
