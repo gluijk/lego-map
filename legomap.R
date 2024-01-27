d70d238f7bfec254ad49686dd114a838e8eb7186
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2024-01-28 00:26:52 +0100

    Add files via upload

--- legomap.R
+++ legomap.R
@@ -19,30 +19,75 @@ arrayresample=function(img, DIMX, DIMY, method='bilinear') {
 }
 
 
+# Function to draw an arbitrarily sized brick
+drawbrick=function(img, xmin, xmax, ymin, ymax,
+                   brick, BRICKSIZE, MIDGRAY, imgcolour) {
+    DARK=0.3
+    LIGHT=0.7
+    
+    imgout=img
+    # Build base grayscale brick
+    for (i in ymin:ymax) {
+        for (j in xmin:xmax)
+            for (chan in 1:3) { imgout[((i-1)*BRICKSIZE+1):(i*BRICKSIZE),
+                                       ((j-1)*BRICKSIZE+1):(j*BRICKSIZE),
+                                       chan]=brick
+        }
+    }
+    
+    # Brick limits in imgout
+    rangeymin = (ymin-1)*BRICKSIZE+1
+    rangeymax = ymax*BRICKSIZE
+    rangexmin = (xmin-1)*BRICKSIZE+1
+    rangexmax = xmax*BRICKSIZE
+    
+    # Draw pseudo 3D borders
+    imgout[rangeymin:(rangeymin+1), rangexmin:rangexmax,]=LIGHT
+    imgout[rangeymin:rangeymax, (rangexmax-1):rangexmax,]=DARK
+    imgout[(rangeymax-1):rangeymax, rangexmin:rangexmax,]=DARK
+    imgout[rangeymin:rangeymax, rangexmin:(rangexmin+1),]=LIGHT
+    imgout[rangeymin, rangexmax-1,]=LIGHT
+    imgout[rangeymax, rangexmin+1,]=DARK
+    
+    # Colour brick according to imgcolour
+    for (chan in 1:3) {
+        gamma=1/(log(imgcolour[ymin, xmin, chan])/log(MIDGRAY))
+        imgout[rangeymin:rangeymax, rangexmin:rangexmax, chan]=
+            imgout[rangeymin:rangeymax, rangexmin:rangexmax, chan]^(1/gamma)
+    }
+
+    return(imgout)
+}
+
+
+
 ###########################################################
 
 # Read and preprocess 1x1 LEGO brick
 brick=readPNG("legobrick46x46.png")  #  46x46 pixels grayscale bitmap
+brick=readPNG("legobrick25x25.png")  #  25x25 pixels grayscale bitmap
 BRICKSIZE=nrow(brick)
 MIDGRAY=median(brick)  # median should be ~0.5 (8-bit 128/255)
 print(paste0("Median of brick vs 128/255: ", MIDGRAY, " vs ", 128/255))
 
-# Add pseudo 3D borders to base brick
-DARK=0.4
-LIGHT=0.6
-brick[1:2, 1:46]=LIGHT
-brick[1:46, 45:46]=DARK
-brick[45:46, 1:46]=DARK
-brick[1:46, 1:2]=LIGHT
-brick[1, 45]=LIGHT
-brick[46, 2]=DARK
-
 
 # List of images to LEGOnize
-name=c('retrato', 'beso', 'guadarrama', 'girasoles', 'africa')
-K=c(8, 4, 7, 8, 10)  # k-means clusters
+name=c('perla', 'gioconda', 'modigliani', 'girasoles', 'picasso',
+       'warhol', 'vangogh', 'urbino', 'katemoss', 'retrato', 'beso',
+       'guadarrama', 'world', 'world2', 'world3', 'world4', 'world5', 'africa')
+K=c(7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7)  # k-means clusters
 
-LEGOSIZE=60  # output vertical size (number of LEGO bricks)
+name=c('sonic')
+K=c(14)  # k-means clusters
+
+name=c('pokemon')
+K=c(4)  # k-means clusters
+
+name=c('tenerife')
+K=c(8)  # k-means clusters
+
+LEGOSIZE=50  # output vertical size (number of LEGO bricks)
+
 
 # Pipeline:
 # img (input) -> imglite (downsized) -> imgclust (clustered) -> imgout (LEGO)
@@ -57,20 +102,23 @@ for (n in 1:length(name)) {
     DIMY=nrow(img)
     DIMX=ncol(img)
     
+    img[img==0]=NA  # ignore blacks in resample to prevent creating mixed colours
     imglite=arrayresample(img, round(LEGOSIZE*DIMX/DIMY), LEGOSIZE)
-    writePNG(imglite, paste0(name[n], "_lite.png"))
+    writePNG(imglite, paste0("lite_", name[n], ".png"))
     
     
     ################################
     # 2. K-MEANS CLUSTERING
-
+    
     DIMX=ncol(imglite)
     DIMY=nrow(imglite)
+    
+    imglite[is.na(imglite)]=0
     
     # Rearrange imglite as a N x 3 array with RGB values in 3 columns
     M=cbind(c(imglite[,,1]), c(imglite[,,2]), c(imglite[,,3]))
     colnames(M)=c("R","G","B")
-
+    
     # Standard k-means clustering
     NCOLOURS=K[n]  # k clusters
     set.seed(0)  # reproducible clustering
@@ -78,9 +126,9 @@ for (n in 1:length(name)) {
                      nstart=1000, iter.max=500)  # high nstart can prevent from
     clustering=kmeansfit$cluster           # missing the tiniest clusters
     centers=kmeansfit$centers  # clustering centroids (average colours)
-
+    
     # Clustering histogram
-    png(paste0(name[n],"_histogram.png"), width=512, height=400)
+    png(paste0("hist_", name[n], ".png"), width=512, height=400)
     breaks=seq(0, NCOLOURS, length.out=NCOLOURS+1)
     colores=rgb(centers[,1], centers[,2], centers[,3])
     hist(clustering, breaks=breaks, col=colores, # lty="blank",
@@ -88,7 +136,7 @@ for (n in 1:length(name)) {
     axis(1, at=breaks, labels=TRUE)
     dev.off()
     
-    # Rebuild clustered image
+    # Build clustered coloured image
     imgclust=array(0, c(DIMY*DIMX,3))  # configure DIMY*DIMX x 3 array
     for (i in 1:NCOLOURS) {  # loop through clusters
         indices=which(clustering==i)
@@ -98,7 +146,7 @@ for (n in 1:length(name)) {
     
     # writeTIFF(imgclust, paste0(name[n],"_clustered.tif"),
     #           bits.per.sample=16, compression="LZW")
-    writePNG(imgclust, paste0(name[n],"_lite_clustered.png"))
+    writePNG(imgclust, paste0("cluster_", name[n], ".png"))
     
     
     ################################
@@ -108,20 +156,48 @@ for (n in 1:length(name)) {
     DIMX=ncol(imgclust)
     imgout=array(0, c(DIMY*BRICKSIZE, DIMX*BRICKSIZE, 3))
     
-    # imgclust=imglite  # to get colour gradients output
+    # imgclust=imglite  # to preserve colour gradients output
     imgclust[imgclust>0.95]=0.95  # clip highlights to prevent whitening
     imgclust[imgclust<0.05]=0.05  # clip shadows to prevent blackening
-    for (i in 1:DIMY) {
-        for (j in 1:DIMX) {
-            for (chan in 1:3) {
-                # Gamma turns brick median (~0.5) into desired pixel colour
-                gamma=1/(log(imgclust[i, j, chan])/log(MIDGRAY))
-                # Vectorized colouring for single brick RGB channel
-                imgout[((i-1)*BRICKSIZE+1):(i*BRICKSIZE),
-                       ((j-1)*BRICKSIZE+1):(j*BRICKSIZE), chan]=brick^(1/gamma)
+
+    # Brute force LEGO brick fitting algorithm
+    LEGOBRICKS=list(c(8,8), c(6,6), c(6,4), c(4,4), c(2,4), c(2,3), c(2,2),
+                    c(4,1), c(3,1), c(2,1), c(1,1))  # hierarchical list
+    NSIZES=length(LEGOBRICKS)
+
+    NBRICKS=0
+    # for (k in 1:NCOLOURS) {  # loop trough clusters
+    for (k in c(1,3,4,5,6,7,8)) {  # ignore cluster 2 (Tenerife's sea)
+        indices=which(clustering==k)
+        imgclust1=array(0, c(DIMY, DIMX))
+        imgclust1[indices]=1  # set to 1 pixels belonging to cluster k
+        for (l in 1:NSIZES) {  # loop trough brick sizes
+            DIMYBRICK=LEGOBRICKS[[l]][1]
+            DIMXBRICK=LEGOBRICKS[[l]][2]
+            AREABRICK=DIMXBRICK*DIMYBRICK
+
+            # Try both 0º and 90º rotation on non-square bricks
+            for (rotate in 1:ifelse(DIMYBRICK==DIMXBRICK, 1, 2)) {
+                for (i in 1:(DIMY-DIMYBRICK+1)) {  # Y axis (rows)
+                    for (j in 1:(DIMX-DIMXBRICK+1)) {  # X axis (cols)
+                        imax=i+DIMYBRICK-1
+                        jmax=j+DIMXBRICK-1
+                        if (sum(imgclust1[i:imax, j:jmax])==AREABRICK) {  # all 1's=brick match
+                            imgout=drawbrick(imgout, j, jmax, i, imax,
+                                             brick, BRICKSIZE, MIDGRAY, imgclust)
+                            imgclust1[i:imax, j:jmax]=0  # remove drawn brick
+                            NBRICKS=NBRICKS+1
+                        }
+                    }
+                }
+                DIMYBRICK=LEGOBRICKS[[l]][2]  # will run only for non-square bricks
+                DIMXBRICK=LEGOBRICKS[[l]][1]
             }
         }
     }
-    # writeTIFF(imgout, paste0(name[n], "_lego.tif"), bits.per.sample=16, compression="LZW")
-    writePNG(imgout, paste0(name[n], "_lego.png"))
+
+    # writeTIFF(imgout, paste0(name[n], "_lego.tif"),
+    #           bits.per.sample=16, compression="LZW")
+    writePNG(imgout, paste0("LEGO_", name[n], ".png"))
+    print(paste0(NBRICKS, " bricks used"))
 }
