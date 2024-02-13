f246dfab49a07daeb6ec462df9b700db685cdb14
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2024-02-13 09:46:43 +0100

    Update legomap_thegrid.R

--- legomap_thegrid.R
+++ legomap_thegrid.R
@@ -3,7 +3,6 @@
 # https://www.overfitting.net/
 
 
-library(tiff)  # save 16-bit TIFF's
 library(png)  # save 8-bit PNG's
 library(terra)  # resample
 
@@ -96,120 +95,6 @@ drawbrick=function(img,
         imgout[rangeymin:rangeymax, rangexmin:rangexmax, chan]^(1/colgamma[chan])
     
     return(imgout)
-}
-
-
-# Functions to write numbers in Inventory
-
-NewBitmap = function(dimx, dimy, val=0) {
-    # Crea bitmap de dimensiones dimx y dimy
-    return(array(val,c(dimx,dimy)))
-}
-
-# Por Carlos Gil Bellosta
-indices.drawline = function(x0, y0, x1, y1) {
-    x0=round(x0)
-    x1=round(x1)
-    y0=round(y0)
-    y1=round(y1)
-    
-    if (y0 == y1) return(cbind(x0:x1, y0)) # Recta de m=0 o un punto
-    if (abs(x1 - x0) >= abs(y1 - y0)) { # Recta de 0 < |m| <= 1
-        m = (y1 - y0) / (x1 - x0)
-        cbind(x0:x1, round(y0 + m * ((x0:x1) - x0)))
-    } else indices.drawline(y0, x0, y1, x1)[, 2:1]  # Recta de |m| > 1
-    # Llamada traspuesta recursiva y traspuesta
-}
-
-DrawLine = function(img, x0, y0, x1, y1, inc=TRUE, val=1) {
-    # Dibuja recta desde (x0,y0)-(x1,y1)
-    # Por defecto m?todo no destructivo y con valor=1
-    indices=indices.drawline(x0, y0, x1, y1)
-    if (inc) img[indices]=img[indices]+val
-    else img[indices]=val
-    
-    return(img)
-}
-
-DrawRect = function(img, x0, y0, x1, y1, inc=TRUE, val=1, fill=FALSE) {
-    # Dibuja rectángulo (x0,y0)-(x1,y1)
-    # Por defecto m?todo no destructivo, con valor=1 y sin relleno
-    x0=round(x0)
-    x1=round(x1)
-    y0=round(y0)
-    y1=round(y1)
-    
-    if (fill) {
-        if (inc) img[x0:x1,y0:y1]=img[x0:x1,y0:y1]+val
-        else img[x0:x1,y0:y1]=val
-        
-        return(img)
-    } else {
-        indices=which( ( (row(img)==x0         | row(img)==x1        ) &
-                             (col(img)>=min(y0,y1) & col(img)<=max(y0,y1)) ) |
-                           ( (col(img)==y0         | col(img)==y1        ) &
-                                 (row(img)>=min(x0,x1) & row(img)<=max(x0,x1)) ) )
-        if (inc) img[indices]=img[indices]+val
-        else img[indices]=val
-        
-        return(img)
-    }
-}
-
-DibujarNumero = function(img, x0, y0, inc=FALSE, val=1, fill=FALSE,
-                         num, width, height) {
-    # Dibuja cifra 0-9 en (x0,y0)
-    # Por defecto método no destructivo y con valor=1
-    
-    if (num=='0') { 
-        img=DrawRect(img, x0, y0, x0+width, y0-height, inc, val, fill)
-    } else if (num=='1') {
-        img=DrawLine(img, x0+width/2, y0, x0+width/2, y0-height, inc, val)
-    } else if (num=='2') {
-        img=DrawLine(img, x0, y0, x0+width, y0, inc, val)
-        img=DrawLine(img, x0+width, y0, x0+width, y0-height/2, inc, val)
-        img=DrawLine(img, x0+width, y0-height/2, x0, y0-height/2, inc, val)
-        img=DrawLine(img, x0, y0-height/2, x0, y0-height, inc, val)
-        img=DrawLine(img, x0, y0-height, x0+width, y0-height, inc, val)
-    } else if (num=='3') {
-        img=DrawLine(img, x0, y0, x0+width, y0, inc, val)
-        img=DrawLine(img, x0, y0-height/2, x0+width, y0-height/2, inc, val)
-        img=DrawLine(img, x0, y0-height, x0+width, y0-height, inc, val)
-        img=DrawLine(img, x0+width, y0, x0+width, y0-height, inc, val)
-    } else if (num=='4') {
-        img=DrawLine(img, x0, y0, x0, y0-height/2, inc, val)
-        img=DrawLine(img, x0, y0-height/2, x0+width, y0-height/2, inc, val)
-        img=DrawLine(img, x0+width, y0, x0+width, y0-height, inc, val)
-    } else if (num=='5') {
-        img=DrawLine(img, x0+width, y0, x0, y0, inc, val)
-        img=DrawLine(img, x0, y0, x0, y0-height/2, inc, val)
-        img=DrawLine(img, x0, y0-height/2, x0+width, y0-height/2, inc, val)
-        img=DrawLine(img, x0+width, y0-height/2, x0+width, y0-height, inc, val)
-        img=DrawLine(img, x0+width, y0-height, x0, y0-height, inc, val)
-    } else if (num=='6') {
-        img=DrawRect(img, x0, y0-height/2, x0+width, y0-height, inc, val, fill)
-        img=DrawLine(img, x0, y0, x0+width, y0, inc, val)
-        img=DrawLine(img, x0, y0, x0, y0-height/2, inc, val)
-    } else if (num=='7') {
-        img=DrawLine(img, x0, y0, x0+width, y0, inc, val)
-        img=DrawLine(img, x0+width, y0, x0+width, y0-height, inc, val)
-    } else if (num=='8') {
-        img=DrawRect(img, x0, y0, x0+width, y0-height/2, inc, val, fill)
-        img=DrawRect(img, x0, y0-height/2, x0+width, y0-height, inc, val, fill)
-    } else if (num=='9') {
-        img=DrawRect(img, x0, y0, x0+width, y0-height/2, inc, val, fill)
-        img=DrawLine(img, x0+width, y0-height/2, x0+width, y0-height, inc, val)
-        img=DrawLine(img, x0, y0-height, x0+width, y0-height, inc, val)
-    } else if (num=='-') {
-        img=DrawLine(img, x0, y0-height/2, x0+width, y0-height/2, inc, val)
-    } else if (num=='x') {
-        img=DrawLine(img, x0, y0-height, x0+width, y0-height/2, inc, val)
-        img=DrawLine(img, x0, y0-height/2, x0+width, y0-height, inc, val)
-    } else {
-        return(img)  # Cifra inválida
-    }
-    
-    return(img)
 }
 
 
@@ -415,14 +300,6 @@ legomap = function(img, name, k=8,
 
 
 
-
-# Examples
-img=readPNG("moto.png")
-inventory=legomap(img, 'moto', k=30,
-          resize=TRUE, LEGOSIZEY=45,
-          background=TRUE, backgroundcolour=c(0, 0, 0))
-
-
 # General animation parameters
 DIMY=1080  # Full HD resolution
 DIMX=1920
@@ -581,7 +458,7 @@ for (frame in 0:(NFRAMES-1)) {
     exp1=((frame/(NFRAMES-1))^(1/gammak1))
     exp2=((frame/(NFRAMES-1))^(1/gammak2))
     expfinal=(exp1-exp2)/2*(sin(2*pi*N*frame/(NFRAMES-1))+1)+exp2
-    k=round( (kMAX-kMIN) * expfinal + kMIN)
+    k=round((kMAX-kMIN) * expfinal + kMIN)
     kseries=c(kseries, k)
 }
 plot(kseries, type='s')
